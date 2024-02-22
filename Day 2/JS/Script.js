6c07651b31dc42ad3f6ccfda1715d369cb4dfcab
Akshay Kolhe <akshaydadu90@gmail.com>
2024-02-22 09:12:47 +0530

    new file:   Day 2/Display.html 	new file:   Day 2/DivSpan.html 	modified:   Day 2/Graphics.html 	new file:   Day 2/JS/OutputJS.html 	new file:   Day 2/JS/Script.js 	new file:   Day 2/JS/TypeJS.html 	new file:   Day 2/MarginPadding.html 	new file:   Day 2/Properties.html 	new file:   Day 2/Selector.html 	new file:   Day 2/Style.css 	new file:   Day 2/TypeCSS.html

---
+++ Day 2/JS/Script.js
@@ -0,0 +1,16 @@
+function Greet() {
+  window.alert("Have a Nice Day!");
+}
+
+function msgOnBody() {
+  document.write("Suraj Sanap is the author of this webpage.");
+}
+
+function msgOnElement() {
+  document.getElementById("msg").innerText =
+    "This is Suraj's Personal Website.";
+}
+
+function msgOnConsole() {
+  console.log("Suraj Sanap is going to start a Startup");
+}
